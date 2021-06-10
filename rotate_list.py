072506ac3ca450d0eb8df5074f1a75a5211f3f07
viswaamitcodes <71846247+viswaamitcodes@users.noreply.github.com>
2021-06-10 11:07:31 +0530

    Update rotate_list.py

--- rotate_list.py
+++ rotate_list.py
@@ -2,8 +2,6 @@ n=int(input())
 data=list(map(int,input().split()))
 rotate=int(input())
 def rotate_list(n,data,rotate):
-    if rotate>len(data):
-        rotate%=len(data)
     for i in data[:rotate]:
         data.remove(i)
         data.append(i)    
